b1db56554885aff8d2c33c76e840043981bb8857
sala7khaled <sala7khalad@gmail.com>
2026-02-28 14:36:22 +0200

    Initial Commit

---
+++ Network/NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkApp.swift
+//  Network
+//
+//  Created by Salah Khaled on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
